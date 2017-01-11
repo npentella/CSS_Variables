d4f60e4938d76f50e5b364d74e1f591df9d8f569
npentella <npentella@gmail.com>
2017-01-11 14:46:47 -0600

    Link inputs to CSS variables, creating desired functionality

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
 const inputs = document.querySelectorAll('input');
 
-console.log(inputs);
+function handleUpdate() {
+	const suffix = this.dataset.sizing || '';
+	document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach(function(input) {
+	input.addEventListener('change', handleUpdate);
+	input.addEventListener('mousemove', handleUpdate);
+}); 
